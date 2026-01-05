477aae2bb6e656c22c48cbf2c302577841204f0b
7aj2aprilio <7aj2aprilio@gmail.com>
2026-01-05 20:16:20 +0700

    google fix

--- config/google_auth.php
+++ config/google_auth.php
@@ -4,11 +4,9 @@ require_once __DIR__ . '/../vendor/autoload.php';
 // Google Client Configuration
 $google_client = new Google_Client();
 
-// You can set these in .env or hardcode for dev (NOT RECOMMENDED for production)
-// For this environment, we'll try to load from env or fallback to empty prompts
-$google_client->setClientId('114430708538-7n4ege0pohcpte3b940ljbl4k4g3f2kg.apps.googleusercontent.com');
-$google_client->setClientSecret('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');
-$google_client->setRedirectUri('https://pawtraitid.azurewebsites.net/google_callback.php');
+$google_client->setClientId(getenv('GOOGLE_CLIENT_ID'));
+$google_client->setClientSecret(getenv('GOOGLE_CLIENT_SECRET'));
+$google_client->setRedirectUri(getenv('GOOGLE_REDIRECT_URI'));
 
 $google_client->addScope('email');
 $google_client->addScope('profile');
