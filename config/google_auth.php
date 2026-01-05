49543d90e5886ff41487e244763bcfff408b085f
7aj2aprilio <7aj2aprilio@gmail.com>
2026-01-05 20:02:46 +0700

    google fix

--- config/google_auth.php
+++ config/google_auth.php
@@ -8,7 +8,7 @@ $google_client = new Google_Client();
 // For this environment, we'll try to load from env or fallback to empty prompts
 $google_client->setClientId('114430708538-7n4ege0pohcpte3b940ljbl4k4g3f2kg.apps.googleusercontent.com');
 $google_client->setClientSecret('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');
-$google_client->setRedirectUri('http://localhost:8000/google_callback.php');
+$google_client->setRedirectUri('https://pawtraitid.azurewebsites.net/google_callback.php');
 
 $google_client->addScope('email');
 $google_client->addScope('profile');
